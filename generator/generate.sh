02f6ac151a6adf41dd9b8bf8f037ae0e83b4636c
Gtker <git@gtker.com>
2024-03-09 09:08:02 +0100

    Add /usr/local/bin prefix to jtd- executables

--- generator/generate.sh
+++ generator/generate.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-jtd-validate ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json ${SCRIPT_DIR}/wow_messages/intermediate_representation.json
-jtd-codegen --python-out ${SCRIPT_DIR} ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json
+/usr/local/bin/jtd-validate ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json ${SCRIPT_DIR}/wow_messages/intermediate_representation.json
+/usr/local/bin/jtd-codegen --python-out ${SCRIPT_DIR} ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json
 mv ${SCRIPT_DIR}/__init__.py ${SCRIPT_DIR}/model.py
 
