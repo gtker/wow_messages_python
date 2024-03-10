41c3cf36aac4b771bbeb93dfdebf727ff4d50147
Gtker <git@gtker.com>
2024-03-10 08:37:03 +0100

    generate: Actually run main.py

--- generator/generate.sh
+++ generator/generate.sh
@@ -7,3 +7,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 /usr/local/bin/jtd-codegen --python-out ${SCRIPT_DIR} ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json
 mv ${SCRIPT_DIR}/__init__.py ${SCRIPT_DIR}/model.py
 
+python3 ${SCRIPT_DIR}/main.py
+
