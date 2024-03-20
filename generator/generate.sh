4f0c1b8e6b76788e953693c981d872ec338c0f5f
Gtker <git@gtker.com>
2024-03-20 10:47:44 +0100

    Run mypy in generator python

--- generator/generate.sh
+++ generator/generate.sh
@@ -7,5 +7,7 @@ jtd-validate ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json 
 jtd-codegen --python-out ${SCRIPT_DIR} ${SCRIPT_DIR}/wow_messages/intermediate_representation_schema.json
 mv ${SCRIPT_DIR}/__init__.py ${SCRIPT_DIR}/model.py
 
+mypy ${SCRIPT_DIR}
+
 python3 ${SCRIPT_DIR}/main.py
 
