3f3ba6eeb3750c78632bb137ee08911a30ec0439
Gtker <git@gtker.com>
2024-05-27 16:14:31 +0200

    Make datetime_re raw string

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ GENERATOR_DIR="${SCRIPT_DIR}/generator"
 jtd-validate ${GENERATOR_DIR}/wow_messages/intermediate_representation_schema.json ${SCRIPT_DIR}/generator/wow_messages/intermediate_representation.json
 jtd-codegen --python-out ${GENERATOR_DIR} ${GENERATOR_DIR}/wow_messages/intermediate_representation_schema.json
 mv ${GENERATOR_DIR}/__init__.py ${GENERATOR_DIR}/model.py
+sed -i 's/datetime_re = /datetime_re = r/' ${GENERATOR_DIR}/model.py
 
 mypy ${GENERATOR_DIR}
 
